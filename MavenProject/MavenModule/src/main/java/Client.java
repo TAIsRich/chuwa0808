5fc0491e270d6654695b41b1b906a3a480b30454
Haide-He <572081272@qq.com>
2022-08-12 17:25:09 -0700

    hw2

---
+++ MavenProject/MavenModule/src/main/java/Client.java
@@ -0,0 +1,16 @@
+public class Client {
+    public static void main(String[] args) {
+        Cricket cricket = new Cricket();
+        Football football = new Football();
+
+        cricket.retirePlayer(1);
+        football.retirePlayer(2);
+
+        int[] age = new int[] {11,2,3,4,5,6,7,8,9,10,11};
+        cricket.calculateAvgAge(age);
+        football.calculateAvgAge(age);
+
+        football.playerTransfer(200, 2);
+        football.playerTransfer(200, 9);
+    }
+}
