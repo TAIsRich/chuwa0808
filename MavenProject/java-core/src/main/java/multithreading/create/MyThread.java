a12249031f442c6573fa8e9f52057fcea721401b
cynthia wang <zwang1916@gmail.com>
2022-08-22 14:28:45 -0400

    hw24

---
+++ MavenProject/java-core/src/main/java/multithreading/create/MyThread.java
@@ -0,0 +1,9 @@
+package multithreading.create;
+
+public class MyThread  extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("start new thread using extends thread");
+    }
+}
