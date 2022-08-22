4da02dd40f39ba5fe7003feaecc79dc6836e0170
valtinaxu <you@example.com>
2022-08-21 21:07:35 -0700

    hw5 practice multithreading

---
+++ ShortQuestions/hw5/multithreading_tutorial/src/c02_basic_handling/JoinTest.java
@@ -0,0 +1,27 @@
+package c02_basic_handling;
+
+public class JoinTest {
+    public static void main(String[] args) {
+
+        Thread t = new Thread(() -> {
+            System.out.println("Thread start");
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println("Thread end");
+        });
+
+        System.out.println("Main start");
+        t.start();
+        try{
+            System.out.println("Main thread is stopped and waiting for t thread end");
+            t.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Main stop");
+    }
+}
