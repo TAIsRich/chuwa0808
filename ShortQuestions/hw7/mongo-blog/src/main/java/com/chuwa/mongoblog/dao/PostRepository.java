be3e8564cbb60dc5a1cda712e12aee2b39a961fe
valtinaxu <you@example.com>
2022-08-24 21:09:59 -0700

    hw7

--- ShortQuestions/hw7/mongo-blog/src/main/java/com/chuwa/mongoblog/dao/PostRepository.java
+++ ShortQuestions/hw7/mongo-blog/src/main/java/com/chuwa/mongoblog/dao/PostRepository.java
@@ -2,6 +2,8 @@ package com.chuwa.mongoblog.dao;
 
 import com.chuwa.mongoblog.entity.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 }
--- ShortQuestions/hw7/mongo-blog/src/main/java/com/chuwa/mongoblog/entity/Post.java
+++ ShortQuestions/hw7/mongo-blog/src/main/java/com/chuwa/mongoblog/entity/Post.java
@@ -1,10 +1,10 @@
 package com.chuwa.mongoblog.entity;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Document(collection = "posts")
