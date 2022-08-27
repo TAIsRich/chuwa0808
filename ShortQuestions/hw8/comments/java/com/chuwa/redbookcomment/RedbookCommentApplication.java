764d5be2631fb1ecfb040abfac8a15badf4071bf
Adam Yin <[adamyin.97@gmail.com]>
2022-08-26 17:11:09 -0700

    hw8

---
+++ ShortQuestions/hw8/comments/java/com/chuwa/redbookcomment/RedbookCommentApplication.java
@@ -0,0 +1,13 @@
+package com.chuwa.redbookcomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbookCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbookCommentApplication.class, args);
+	}
+
+}
