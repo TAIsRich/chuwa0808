961f18877d2f80a71937ffe1aee09836dd32941c
valtinaxu <you@example.com>
2022-08-30 20:51:11 -0700

    search function

---
+++ RedBook/redbook - CRUD +search/src/main/java/com/chuwa/redbook/dao/PostRepository.java
@@ -0,0 +1,12 @@
+package com.chuwa.redbook.dao;
+
+import com.chuwa.redbook.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long > {
+    List<Post> searchPostsByTitleContainsOrDescriptionContainsOrContentContains(String title, String description, String content);
+}
