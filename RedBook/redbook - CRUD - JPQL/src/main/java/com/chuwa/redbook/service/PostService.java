7f60e21d1dcae76985b59833fe44a7fd69bffc30
valtinaxu <you@example.com>
2022-08-31 13:13:37 -0700

    redbook - JPQL

---
+++ RedBook/redbook - CRUD - JPQL/src/main/java/com/chuwa/redbook/service/PostService.java
@@ -0,0 +1,18 @@
+package com.chuwa.redbook.service;
+
+import com.chuwa.redbook.payload.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+
+    PostDto createPst(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatPost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
