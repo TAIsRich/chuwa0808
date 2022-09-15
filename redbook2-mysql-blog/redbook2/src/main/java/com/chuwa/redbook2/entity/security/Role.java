c48a79d454feb89064cfab9f83555229ea7ba0eb
Xuan Pan <theresa@xuans-air.lan>
2022-09-14 17:27:23 -0700

    theresa_pan/hw14

---
+++ redbook2-mysql-blog/redbook2/src/main/java/com/chuwa/redbook2/entity/security/Role.java
@@ -0,0 +1,41 @@
+package com.chuwa.redbook2.entity.security;
+
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ redbook2-mysql-blog/redbook2/src/main/java/com/chuwa/redbook2/payload/security/JWTAuthResponse.java
@@ -0,0 +1,28 @@
+package com.chuwa.redbook2.payload.security;
+
+public class JWTAuthResponse {
+
+    private String accessToken;
+    private String tokenType;
+
+    public JWTAuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+}
